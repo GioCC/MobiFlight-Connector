6b09f80058e389094267d09b31eb6263bf83bca4
GioCC <25667790+GioCC@users.noreply.github.com>
2022-11-07 09:40:47 +0100

    WIP - Added skeleton of new PinAssignPanel class

---
+++ UI/Panels/Config/PinAssignPanel.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace MobiFlight.UI.Panels.Config
+{
+    partial class PinAssignPanel
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpBase = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flpBase
+            // 
+            this.flpBase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpBase.Location = new System.Drawing.Point(0, 0);
+            this.flpBase.Name = "flpBase";
+            this.flpBase.Size = new System.Drawing.Size(304, 155);
+            this.flpBase.TabIndex = 9;
+            // 
+            // PinAssignPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flpBase);
+            this.Name = "PinAssignPanel";
+            this.Size = new System.Drawing.Size(304, 155);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flpBase;
+    }
+}
---
+++ UI/Panels/Config/PinAssignPanel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MobiFlight.UI.Panels.Config
+{
+    public partial class PinAssignPanel : UserControl
+    {
+        byte _byte = 1;
+        List<MobiFlightPin> _pinList;
+
+        public PinAssignPanel(List<MobiFlightPin> pinList)
+        {
+            InitializeComponent();
+            rebuild(pinList);
+        }
+
+        public void rebuild(List<MobiFlightPin> pinList)
+        {
+            _pinList = pinList;
+            _build(pinList);
+        }
+
+        private void _build(List<MobiFlightPin> pinList)
+        {
+            flpBase.Controls.Clear();
+            _pinList?.ForEach(pin => _addPinBox(pin));
+
+        }
+
+        private void _addPinBox(MobiFlightPin pin)
+        {
+            CheckBox c = new CheckBox();
+            flpBase.Controls.Add(c);
+        }
+
+        [Description("Set the Byte"), Category("Default")] 
+        public byte Byte
+        {
+            get { return _byte; }
+            set
+            {
+                _byte = value;
+                Label.Text = "Byte " + _byte;
+                for (int i = 0; i != 8; i++)
+                {
+                    flpBase.Controls["checkBox" + i].Text = ((_byte-1) * 8 + i).ToString();
+                }
+            }
+        }
+
+        public byte Value
+        {
+            get { 
+                byte result = 0;
+                for (int i = 0; i != 8; i++)
+                {
+                    result += (Byte)(
+                                ((flpBase.Controls["checkBox" + i] as CheckBox).Checked ? 1 : 0) << i
+                              );
+                }
+                return result;
+            }
+            set
+            {
+                for (int i = 0; i != 8; i++)
+                {
+                    (flpBase.Controls["checkBox" + i] as CheckBox).Checked = ((value & 1) == 1);
+                    value = (byte) (value >> 1);
+                }
+            }
+        }
+    }
+}
